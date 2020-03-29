130dc2b9d9a89bb2276b70f128c7bd89e8646941
ouyangjun <ouyangjun92@qq.com>
2020-03-29 10:03:26 +0800

    单例设计模式

---
+++ app/crontab.php
@@ -0,0 +1,6 @@
+<?php
+class crontab{
+    private const YIELD_KEY = 'yield:list';
+    private const YIELD_DATA_KEY = 'yield:data';
+
+}
---
+++ app/patterns/singleton.php
@@ -0,0 +1,25 @@
+<?php
+
+class Singleton{
+    /**
+     * @desc 自身实例
+     */
+    private static $instance;
+
+    /**
+     * @desc 
+     */
+    public function __clone(){
+        // 禁止单例对象的clone
+    }
+
+    /**
+     * @desc 获取实例
+     */
+    public static function getInstance(){
+        if(self::$instance instanceof self){
+            self::$instance = new self;
+        }
+        return self::$instance
+    }
+}
